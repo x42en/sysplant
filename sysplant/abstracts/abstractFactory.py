775c07c3c54f62dcc02d7ef83e57e9b057c39262
Ben Mz <x42en@users.noreply.github.com>
2023-07-13 18:25:56 +0200

    Update docstring

--- sysplant/abstracts/abstractFactory.py
+++ sysplant/abstracts/abstractFactory.py
@@ -4,7 +4,6 @@ import re
 import string
 import random
 import hashlib
-import logging
 
 from typing import Union
 
@@ -18,16 +17,22 @@ class AbstractFactory(ABC):
     Public Abstract factory class handling standard methods used by child instances
     """
 
-    def __init__(self, log_level: int = logging.INFO) -> None:
+    def __init__(self) -> None:
+        """
+        Init method.
+        Register a self.data var containing the raw data that will be used for operations
+        Setup a Logger singleton to self.logger shared var usable by child clases
+        """
         super().__init__()
 
         self.data: str = ""
 
         # Share logger for all childs
-        self.logger = LoggerSingleton(log_level)
+        self.logger = LoggerSingleton()
 
     def replace_tag(self, name: str, content: str) -> None:
-        """Public method used to replace a TAG pattern in data var with content
+        """
+        Public method used to replace a TAG pattern in data var with content
 
         Args:
             name (str): the tag name to replace
@@ -38,7 +43,8 @@ class AbstractFactory(ABC):
         )
 
     def remove_tag(self, name: str) -> None:
-        """Public method used to remove a TAG pattern in data var (dropping the line for clean code at the same time)
+        """
+        Public method used to remove a TAG pattern in self.data var (dropping the line for clean code at the same time)
 
         Args:
             name (str): the tag name to remove
@@ -52,6 +58,15 @@ class AbstractFactory(ABC):
     def __replace_pattern(
         self, pattern: str, content: Union[str, int], count: int = -1
     ) -> None:
+        """
+        Private method used by replace_tag()
+        Wille replace pattern value by content in self.data var
+
+        Args:
+            pattern (str): Tag value to search
+            content (Union[str, int]): Content to replace with
+            count (int, optional): Number of time to replace with. Defaults to -1.
+        """
         # Replace tag with content in template data
         self.data = self.data.replace(
             pattern,
@@ -60,7 +75,8 @@ class AbstractFactory(ABC):
         )
 
     def generate_random_seed(self) -> int:
-        """Public method used to generate a random int used as seed
+        """
+        Public method used to generate a random int used as seed
         Range from: 2^28 to (2^32 - 1)
 
         Returns:
@@ -71,19 +87,22 @@ class AbstractFactory(ABC):
     def generate_random_string(
         self, length: int, choices: list = string.ascii_letters
     ) -> str:
-        """Private method used to generate a random string
+        """
+        Public method used to generate a random string
 
         Args:
             length (int): The random string length to generate
-            choices (list, optional): The chars sapce to used for generation. Defaults to string.ascii_letters.
+            choices (list, optional): The chars space used for generation. Defaults to string.ascii_letters.
 
         Returns:
             str: The generated random string
         """
-        return "".join(random.choice(choices) for _ in range(length))
+        clean = [str(x) for x in choices]
+        return "".join(random.choice(clean) for _ in range(length))
 
     def get_function_hash(self, seed: int, name: str) -> int:
-        """Public method used to hash kernel call to evade with random hex number in order to avoid static analysis detection.
+        """
+        Public method used to hash kernel call to evade with random hex number in order to avoid static analysis detection.
 
         Args:
             name (str): The kernel call to evade
