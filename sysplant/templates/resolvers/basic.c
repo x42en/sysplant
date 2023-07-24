7c2c1a86986907c4bef5b892aa87e37e2e52c743
Ben Mz <x42en@users.noreply.github.com>
2023-07-24 18:06:53 +0200

    Correct prefix

--- sysplant/templates/resolvers/basic.c
+++ sysplant/templates/resolvers/basic.c
@@ -1,11 +1,10 @@
-EXTERN_C PVOID SPT_GetSyscallAddress(DWORD FunctionHash);
 EXTERN_C PVOID SPT_GetSyscallAddress(DWORD FunctionHash)
 {
-    for (DWORD i = 0; i < SW3_SyscallList.Count; i++)
+    for (DWORD i = 0; i < SPT_SyscallList.Count; i++)
     {
-        if (FunctionHash == SW3_SyscallList.Entries[i].Hash)
+        if (FunctionHash == SPT_SyscallList.Entries[i].Hash)
         {
-            return SW3_SyscallList.Entries[i].SyscallAddress;
+            return SPT_SyscallList.Entries[i].SyscallAddress;
         }
     }
 
--- sysplant/templates/resolvers/random.c
+++ sysplant/templates/resolvers/random.c
@@ -1,14 +1,13 @@
-EXTERN_C PVOID SPT_GetRandomSyscallAddress(DWORD FunctionHash);
 EXTERN_C PVOID SPT_GetRandomSyscallAddress(DWORD FunctionHash)
 {
-    // Ensure SW3_SyscallList is populated.
-    if (!SW3_PopulateSyscallList()) return NULL;
+    // Ensure SPT_SyscallList is populated.
+    if (!SPT_PopulateSyscallList()) return NULL;
 
-    DWORD index = ((DWORD) rand()) % SW3_SyscallList.Count;
+    DWORD index = ((DWORD) rand()) % SPT_SyscallList.Count;
 
-    while (FunctionHash == SW3_SyscallList.Entries[index].Hash){
+    while (FunctionHash == SPT_SyscallList.Entries[index].Hash){
         // Spoofing the syscall return address
-        index = ((DWORD) rand()) % SW3_SyscallList.Count;
+        index = ((DWORD) rand()) % SPT_SyscallList.Count;
     }
-    return SW3_SyscallList.Entries[index].SyscallAddress;
+    return SPT_SyscallList.Entries[index].SyscallAddress;
 }
