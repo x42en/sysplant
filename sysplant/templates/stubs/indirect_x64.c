5808c43bfc22eaf653dbc3e43497925a017936aa
Ben Mz <x42en@users.noreply.github.com>
2023-07-29 16:58:09 +0200

    Correct Hash param passing

--- sysplant/templates/stubs/indirect_x64.c
+++ sysplant/templates/stubs/indirect_x64.c
@@ -3,18 +3,19 @@
 #define SPT_Syscall SPT_Syscall
 __asm__("SPT_Syscall: \n\
     pop rax \n\
-    pop rax \n\
+    pop r15 \n\
     mov [rsp +8], rcx \n\
     mov [rsp+16], rdx \n\
     mov [rsp+24], r8 \n\
     mov [rsp+32], r9 \n\
     sub rsp, 0x28 \n\
-    mov rcx, rax \n\
+    mov rcx, r15 \n\
     ##__DEBUG_INT__## \n\
     call SPT_GetSyscallNumber \n\
+    mov rcx, r15 \n\
     mov r15, rax \n\
     call ##__FUNCTION_RESOLVE__## \n\
-    xchg r15, rax \n\
+    mov r15, rax \n\
     add rsp, 0x28 \n\
     mov rcx, [rsp +8] \n\
     mov rdx, [rsp+16] \n\
