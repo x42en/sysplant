5d52dd5da78bfe9f52048f1892dc430a251e3102
Ben Mz <x42en@users.noreply.github.com>
2023-07-30 18:18:08 +0200

    Correct ASM stub

--- sysplant/templates/stubs/random_x64.c
+++ sysplant/templates/stubs/random_x64.c
@@ -13,13 +13,14 @@ __asm__("SPT_Syscall: \n\
     call SPT_GetSyscallNumber \n\
     mov rcx, r15 \n\
     mov r15, rax \n\
-    call ##__FUNCTION_RESOLVE__## \n\
-    mov r15, rax \n\
+    call SPT_GetRandomSyscallAddress \n\
+    xchg r15, rax \n\
     add rsp, 0x28 \n\
     mov rcx, [rsp +8] \n\
     mov rdx, [rsp+16] \n\
     mov r8, [rsp+24] \n\
     mov r9, [rsp+32] \n\
+    mov r10, rcx \n\
     ##__DEBUG_INT__## \n\
     jmp r15 \n\
 ");
